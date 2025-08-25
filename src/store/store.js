c3dc54927d63d2d81796f4d40ad6acbff309f1e1
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-08-25 20:11:33 +0300

    изменил стор

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import gameSlice from './slice'
 import authReducer from './authSlice';
 import ticTacToeReducer from './ticTacToeSlice'
+import game2048Reducer from './game2048slice';
 
 export default configureStore({
   reducer: {
     ticTacToe: ticTacToeReducer,
     game: gameSlice,
-    auth: authReducer
+    auth: authReducer,
+    game2048: game2048Reducer
   },
 })
